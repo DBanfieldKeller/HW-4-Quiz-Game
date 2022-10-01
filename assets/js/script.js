aca3299e2023e5b778743864bcf05537d335e974
Goat <dpajbk@gmail.com>
2022-10-01 15:29:05 -0400

    start button functioning, transitions to first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 // DATA
 var startButton = document.getElementById("start-button");
+var answerButtons =document.getElementById("answer-buttons");
+var question = document.getElementById("question");
 
 // Start Game Function
 startButton.addEventListener('click', function(){
-    console.log("Started")
+    console.log("Started");
+    startButton.setAttribute("class","hide");
+    answerButtons.setAttribute("class","show-buttons");
+    question.innerHTML= "Question 1";
+
 })
