5593692e3b49c04d94d19f8141735aadb51184b8
Goat <dpajbk@gmail.com>
2022-10-02 14:16:21 -0400

    test commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,33 @@ var highScoreButton = document.getElementById("high-score-button");
 // set starting time
 var timeLeft = 5;
 
+// question data base 
+var questionSet = [
+    {
+        question1: {
+            question: "Which of the following is NOT a data type in javascript?",
+            answers: [
+                {text: "Number", correct: false},
+                {text: "Boolean", correct: false},
+                {text: "Variable", correct: true},
+                {text: "String", correct: false},
+            ]
+        }
+    },
+    {   question2: { 
+            question: "What would be the result of 3+2+'7'?",
+            answers: [
+                {text: "12", correct: false},
+                {text: "57", correct: true },
+                {text: "39", correct: false},
+                {text: "-2", correct: false}
+            ]
+
+        }
+
+    }
+]
+
 // Start Game Function
 startButton.addEventListener('click', function(){
     console.log("Started");
@@ -34,3 +61,14 @@ function countdownScore () {
 }
     // TODO when correct answer timer +10
     // TODO when incorrect answer timer -10
+
+// choose question to ask next
+// Fisher-Yates algorithm for shuffling question array//
+function setNextQuestion() {
+
+}
+
+// display question
+function showQuestion() {
+
+}
